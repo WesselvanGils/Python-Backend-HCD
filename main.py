bacf6664d96c10b2baa1e81035b0e2ed717b3ce6
TheElementalDragon <wesselwammes@hotmail.com>
2023-10-27 17:19:25 +0200

    it works now

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
-import torch
+import os
 import json
 import numpy as np
 
-from model import Net
-from flask import Flask, request
+from model import Model
 from flask_cors import CORS
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-HIDDEN_SIZE = 100
-HIDDEN_COUNT = 2
-
-net = Net(20, HIDDEN_SIZE, HIDDEN_COUNT, 2)
-net.load_state_dict(torch.load("./models/model.pth"))
-
+model = Model()
 
 @app.route("/api/divorce", methods=["POST"])
 def handle_post_request():
+    # Convert the data to a numpy array
     post_data = request.get_data()
     post_data_str = post_data.decode("utf-8")
     json_data = json.loads(post_data_str)
     values_array = list(json_data.values())
-    numpy_array = np.array(values_array)
+    numpy_array = np.array(values_array, dtype=np.int32)
 
-    tensor = torch.Tensor(numpy_array)
+    # Make the prediction and generate the image
+    prediction = model.predict(numpy_array)[0]
 
-    with torch.no_grad():
-        outputs = net(tensor)
-        probs = torch.nn.functional.softmax(outputs, dim=0)
-        conf, classes = torch.max(probs, 0)
-        class_names = '0123456789'
-        confidence = conf.item()
-        prediction = class_names[classes.item()]
-
-    json_string = '{"prediction":' + prediction + ","
-    other = '"confidence":' + f'{confidence * 100:.2f}%' + "}"
-
-    json_string += other
-
+    # Generate the JSON response
+    json_string = '{"prediction":' + prediction + '}'
     return f"{json_string}", 200
+
+@app.route("/image", methods=["GET"])
+def handle_image_request():
+    return send_file("./temp.png", mimetype="image/png")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- model.py
+++ model.py
@@ -1,22 +1,47 @@
-import torch.nn as nn
+import shap
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
 
-class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, hidden_count, num_classes):
-        super().__init__()
+from sklearn.model_selection import train_test_split
 
-        self.stack = nn.Sequential()
-        self.stack.add_module("fcfirst", nn.Linear(input_size, hidden_size))
-        self.stack.add_module(f"relufirst", nn.ReLU())
+class Model:
+    def __init__(self):
+        self.X_train = None
+        self.features = None
+        self.model = self.train()
 
-        for i in range(hidden_count):
-            self.stack.add_module(
-                f"fc{i}", nn.Linear(hidden_size, hidden_size))
-            self.stack.add_module(f"relu{i}", nn.ReLU())
+    def train(self):
+        data = pd.read_csv("./data/divorce_data.csv", sep=";")
+        data = pd.concat([data.iloc[:, 0:20], data.iloc[:, -1]], axis=1) 
 
-        self.stack.add_module("fclast", nn.Linear(hidden_size, num_classes))
-        self.stack.add_module("last", nn.Softmax(dim=0))
+        X = data.drop("Divorce", axis=1).to_numpy()
+        y = data["Divorce"].to_numpy()
 
-    def forward(self, x):
-        out = self.stack(x)
-        return out
+        self.features = data.columns[:-1]
+
+        self.X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        mlp = MLPRegressor(hidden_layer_sizes=(5,),activation='logistic', max_iter=10000,learning_rate='invscaling',random_state=0)
+        model = make_pipeline(StandardScaler(), mlp)
+
+        model.fit(self.X_train, y_train)
+        return model
+
+    def predict(self, data):
+        data = data.reshape(1, -1)
+        pred = self.model.predict(data)
+
+        plt.clf()
+
+        explainer = shap.KernelExplainer(self.model.predict, self.X_train[:100])
+        shap_values = explainer.shap_values(data, nsamples=100)
+        shap.force_plot(explainer.expected_value, shap_values, self.X_train[0,:],feature_names=self.features, matplotlib=True, show=False)
+
+        plt.savefig('temp.png')
+        plt.close()
+
+        return pred.astype(str)
